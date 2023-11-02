75b4cd768cc69a609c2d1407d54de15b29371150
miquel227 <miqueldieguez@iesebre.com>
2023-11-02 19:05:50 +0100

    harvester

--- Fase de reconeixement/Harvester.py
+++ Fase de reconeixement/Harvester.py
@@ -1,10 +1,7 @@
-#import subprocess
-#objectiu = input("Introduïu l'objectiu (domini, correu electrònic, etc.): ")
-#opcions = input("Introduïu les opcions de The Harvester (p. ex., -d per domini): ")
-
-#comanda = f"theharvester -d {objectiu} {opcions}"
-#subprocess.call(comanda, shell=True)
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from bot import TelegramBot 
 import subprocess
 
 objectiu = input("Introduïu l'objectiu (domini, correu electrònic, etc.): ")
@@ -13,10 +10,22 @@ opcions = input("Introduïu les opcions de The Harvester (p. ex., -l 5 -b bing):
 # Utilizamos la ruta completa al script theHarvester.py
 comanda = f"python3.10 /home/alumne/theHarv/theHarvester/theHarvester.py -d {objectiu} {opcions}"
 
-# Intentamos ejecutar la comanda
+# Nombre del archivo de salida
+output_file = "resultats_theharvester.txt"
+
+# Intentamos ejecutar la comanda y guardar la salida en el archivo
 try:
-    subprocess.run(comanda, shell=True, check=True)
+    with open(output_file, "w") as output:
+        subprocess.run(comanda, shell=True, check=True, stdout=output)
 except subprocess.CalledProcessError as e:
     print(f"Error al ejecutar The Harvester: {e}")
 except FileNotFoundError:
     print("Error: The Harvester no se encuentra en la ruta. ¿Está instalado?")
+
+# Verificar si se ha creado el archivo y enviarlo al bot de Telegram
+if os.path.exists(output_file):
+    mi_bot = TelegramBot()
+    mi_bot.enviar_document(output_file)
+    os.remove(output_file)
+else:
+    print("No se ha generado el archivo de resultados.")
