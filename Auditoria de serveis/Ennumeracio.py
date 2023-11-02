4d2df99e30f15a6a2a1c43e9f475fa11b6d03f9f
miquel227 <miqueldieguez@iesebre.com>
2023-11-02 19:19:29 +0100

    Bot

--- Auditoria de serveis/Ennumeracio.py
+++ Auditoria de serveis/Ennumeracio.py
@@ -1,4 +1,27 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import subprocess
+from bot import TelegramBot
 
-ip = input("Introdueix la ip que vols escaneijat amb enum4linux:")
-subprocess.run(["enum4linux", "-a", ip])
+ip = input("Introdueix la ip que vols escanejar amb enum4linux: ")
+
+# Nombre del archivo de salida
+output_file = "resultats_enum4linux.txt"
+
+# Intentamos ejecutar enum4linux y guardar la salida en el archivo
+try:
+    with open(output_file, "w") as output:
+        subprocess.run(["enum4linux", "-a", ip], stdout=output)
+except subprocess.CalledProcessError as e:
+    print(f"Error al ejecutar enum4linux: {e}")
+except FileNotFoundError:
+    print("Error: enum4linux no se encuentra en la ruta. ¿Está instalado?")
+
+# Verificar si se ha creado el archivo y enviarlo al bot de Telegram
+if os.path.exists(output_file):
+    mi_bot = TelegramBot()
+    mi_bot.enviar_document(output_file)
+    os.remove(output_file)
+else:
+    print("No se ha generado el archivo de resultados.")
