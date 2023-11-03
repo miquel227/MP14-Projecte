f3b792ee09f088eec06e7719243f87c3728b80ec
miquel227 <miqueldieguez@iesebre.com>
2023-11-03 15:55:49 +0100

    canvis en auditssh

--- Auditoria de serveis/auditoriassh.py
+++ Auditoria de serveis/auditoriassh.py
@@ -24,15 +24,17 @@ def ssh_audit_all_ports(target):
         return "No s'ha trobat cap servidor SSH en cap port."
     
     return "\n".join([f"Port {port} és utilitzat per SSH." for port in used_ports])
-# ... Resta del teu codi ...
 
 def ssh_audit():
     while True:
         print("Benvingut a l'eina d'auditoria SSH!")
         print("Què vols fer amb ssh-audit?")
-        print("1. Executar una auditoria bàsica.")
-        print("2. Executar una auditoria completa.")
-        print("3. Tornar al menú principal.")
+        print("1. Escanejar un servidor SSH específic")
+        print("2. Escaneig forçant ipv4")
+        print("3. Escaneig amb depuració (debug) ")
+        print("4. Mostrar la sortida en format JSON")
+        print("5. Escaneijar tots el ports SSH obersts")
+        print("6. Tornar al menú principal.")
 
         opcio = input("Introdueix el número de l'opció que vols: ")
 
@@ -53,12 +55,51 @@ def ssh_audit():
             os.system('clear' if os.name == 'posix' else 'cls')
             target = input("Introdueix la IP o nom de l'amfitrió SSH que vols auditar: ")
             current_directory = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-            output_file = os.path.join(current_directory, "AuditoriaCompleta.txt")
-            output_text = ssh_audit_all_ports(target)  # Obtenir la sortida
+            output_file = os.path.join(current_directory, "AuditoriaForçantIPv4.txt")
             with open(output_file, "w") as output:
-                output.write(output_text)  # Guardar la sortida al fitxer
+                subprocess.run(["ssh-audit", "-4", target], stdout=output)
+            with open(output_file, "r") as file:
+                print("Contingut del fitxer:")
+                print(file.read())
+            mi_bot = TelegramBot()
+            mi_bot.enviar_document(output_file)
+            os.remove(output_file)
+        elif opcio == '3':
+            os.system('clear' if os.name == 'posix' else 'cls')
+            target = input("Introdueix la IP o nom de l'amfitrió SSH que vols auditar: ")
+            current_directory = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+            output_file = os.path.join(current_directory, "AuditoriaPortDiferent.txt")
+            with open(output_file, "w") as output:
+                subprocess.run(["ssh-audit", "-d", target], stdout=output)
+            with open(output_file, "r") as file:
+                print("Contingut del fitxer:")
+                print(file.read())
+            mi_bot = TelegramBot()
+            mi_bot.enviar_document(output_file)
+            os.remove(output_file)
+        elif opcio == '4':
+            os.system('clear' if os.name == 'posix' else 'cls')
+            target = input("Introdueix la IP o nom de l'amfitrió SSH que vols auditar: ")
+            current_directory = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+            output_file = os.path.join(current_directory, "AuditoriaJSON.txt")
+            with open(output_file, "w") as output:
+                subprocess.run(["ssh-audit", "-j", target], stdout=output)
+            with open(output_file, "r") as file:
+                print("Contingut del fitxer:")
+                print(file.read())
+            mi_bot = TelegramBot()
+            mi_bot.enviar_document(output_file)
+            os.remove(output_file)
+        elif opcio == '5':
+            os.system('clear' if os.name == 'posix' else 'cls')
+            target = input("Introdueix la IP o nom de l'amfitrió SSH que vols auditar: ")
+            current_directory = os.path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+            output_file = os.path.join(current_directory, "PortsSSH_Oberts.txt")
+            output_text = ssh_audit_all_ports(target)  
+            with open(output_file, "w") as output:
+                output.write(output_text)  
             print("Contingut del fitxer:")
-            print(output_text)  # Mostrar la sortida
+            print(output_text)  
             mi_bot = TelegramBot()
             mi_bot.enviar_document(output_file)
             os.remove(output_file)
@@ -70,7 +111,7 @@ def ssh_audit():
 
 if __name__ == "__main__":
     while True:
-        os.system('clear' if os.name == 'posix' else 'cls')  # Esborra la pantalla
+        os.system('clear' if os.name == 'posix' else 'cls')
         print("Quina eina d'auditoria vols utilitzar?")
         print("1. Auditoria SSH amb ssh-audit")
         print("2. Sortir")
