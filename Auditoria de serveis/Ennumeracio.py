426cbd791c40bd8d78d3f1c49fe9761ef5dbb33a
miquel227 <124708580+miquel227@users.noreply.github.com>
2023-11-22 21:13:41 +0100

    Enum4linux funcionant sol a docker imatge no en VSC

--- Auditoria de serveis/Ennumeracio.py
+++ Auditoria de serveis/Ennumeracio.py
@@ -1,24 +1,38 @@
 import os
 import sys
+import time
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import subprocess
 from bot import TelegramBot
 
-ip = input("Introdueix la ip que vols escanejar amb enum4linux: ")
+ip = input("Introdueix la IP que vols escanejar amb enum4linux: ")
 
 # Nombre del archivo de salida
 output_file = "resultats_enum4linux.txt"
 
-# Intentamos ejecutar enum4linux y guardar la salida en el archivo
+# Ruta completa de l'executable enum4linux
+#enum4linux_path = "/snap/bin/enum4linux"
+
+# Intentem executar enum4linux i guardar la sortida a l'arxiu
+print("Abans de l'execució d'enum4linux")
 try:
     with open(output_file, "w") as output:
-        subprocess.run(["enum4linux", "-a", ip], stdout=output)
+        subprocess.run(["/app/MP14-Projecte/enum4linux/enum4linux.pl", "-a", ip], stdout=output)
+        time.sleep(1)
 except subprocess.CalledProcessError as e:
-    print(f"Error al ejecutar enum4linux: {e}")
+    print(f"Error al executar enum4linux: {e}")
 except FileNotFoundError:
     print("Error: enum4linux no se encuentra en la ruta. ¿Está instalado?")
+print("Després de l'execució d'enum4linux")
 
-# Verificar si se ha creado el archivo y enviarlo al bot de Telegram
+# Afegim missatges de depuració addicionals
+print("Abans de llegir el contingut de l'arxiu")
+with open(output_file, "r") as output:
+    content = output.read()
+    print("Contingut de l'arxiu:", content)
+print("Després de llegir el contingut de l'arxiu")
+
+# Verifiquem si s'ha creat l'arxiu i l'enviem al bot de Telegram
 if os.path.exists(output_file):
     mi_bot = TelegramBot()
     mi_bot.enviar_document(output_file)
